a1945c171769232009708b1714b5dd438e1b4049
Maik Schmaddebeck <smadback@gmail.com>
2016-06-07 07:54:51 +0200

    fertig

--- src/main/java/edu/tuc/bsvs/rockpaperscissors/helper/Identifier.java
+++ src/main/java/edu/tuc/bsvs/rockpaperscissors/helper/Identifier.java
@@ -1,8 +1,5 @@
 package edu.tuc.bsvs.rockpaperscissors.helper;
 
-/**
- * Created by Smadback on 24.05.2016.
- */
 public enum Identifier {
     INIT, INFO, ERROR, GAMEOVER, CHOICE, AGAIN, CONNECTION
 }
